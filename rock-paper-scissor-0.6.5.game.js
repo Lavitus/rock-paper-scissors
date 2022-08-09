d24e3931a72032872e28ee1baeddc41708f9804d
LavituScuperino <samet71simsek@gmail.com>
2022-08-09 22:22:31 +0200

    changed gameChoice(array)
    +gameChoice holding strings with numeric values
    now (move choice) player input & computerRandomChoice now finalize
    into a number to be later compared in playRound() to conclude the
    winner of the round.
    
    -playRound() is in the works.

--- rock-paper-scissor-0.6.5.game.js
+++ rock-paper-scissor-0.6.5.game.js
@@ -1,36 +1,29 @@
-let gameChoices = ["Rock", "Paper", "Scissor"];
-let computerChoice = null;
+const gameChoices = [{choice: 'Rock', value: 0}, {choice: 'Paper', value: 1}, {choice: 'Scissors', value: 2}];
+let computerRandomChoice = null;
 let playerChoice = null;
 // computer randomly selects move
 getComputerChoice();
 function getComputerChoice() { 
-computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+computerRandomChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 }
 
 game(); 
 function game () {
     // ask player for text input for move choice to start the round  
-    playerChoice = prompt("Choose your move! Rock, Paper or Scissor?", "");
+    playerChoice = prompt("Choose your move! Rock, Paper or Scissors?", "");
     if (typeof playerChoice === 'string') {
     playerChoice = playerChoice.charAt(0).toUpperCase() +playerChoice.slice(1).toLowerCase();
     }
-    // if no input or player cancels, prompt again
+    // if player cancels or enters no input, ask player again
     if (playerChoice === null) {
         game();
-    } else if (playerChoice.length === 0 ) {
+    }else if (playerChoice.length === 0 ) {
         game()
-    } // start round if player made choice 
-    else if (playerChoice === "Rock" || 
-             playerChoice === "Paper"||
-             playerChoice === "Scissor") {
-        concludeRound();
-    } else { 
-        game();
+    }// start round if player made choice 
+    else { 
+     playerChoice = gameChoices.findIndex(choice => (playerChoice));
+     console.log(playerChoice);
     }
+    
 }
 
-function concludeRound() {
-        if (playerChoice.toLowerCase === computerChoice.toLowerCase) {
-            alert(`You chose ${playerChoice} vs ${computerChoice}, that's Draw!`);
-        }
-    }
