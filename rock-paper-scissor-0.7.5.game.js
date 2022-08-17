8d550d74622d9eb54883bf20d6736a815a29a9fd
LavituScuperino <samet71simsek@gmail.com>
2022-08-17 21:15:23 +0200

    basic game running, points still missing
    +computer automatically choosing move
    
    +'playerChoice' making use of 'gameMoveOptions' array, holds now
    object with 2 values, same for computerChoice.
    
    +added 'playRound()', runs round
    
    +'playerWinCombo' conditions for player winning a round
    (get value out of object, playerChoice.value & computerChoice.value)
    * checks if both values are the same (tie)
    *if no winning condition is met (lose)

--- rock-paper-scissor-0.7.5.game.js
+++ rock-paper-scissor-0.7.5.game.js
@@ -1,29 +1,46 @@
-const gameChoices = [{choice: 'Rock', value: 0}, {choice: 'Paper', value: 1}, {choice: 'Scissors', value: 2}];
-let computerRandomChoice = null;
-let playerChoice = null;
-// computer randomly selects move
-getComputerChoice();
-function getComputerChoice() { 
-computerRandomChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-}
+// rock paper scissors move options
+const gameMoveOptions = [{choice: 'Rock', value: 0}, {choice: 'Paper', value: 1}, {choice: 'Scissors', value: 2}];
 
-game(); 
-function game () {
-    // ask player for text input for move choice to start the round  
-    playerChoice = prompt("Choose your move! Rock, Paper or Scissors?", "");
-    if (typeof playerChoice === 'string') {
-    playerChoice = playerChoice.charAt(0).toUpperCase() +playerChoice.slice(1).toLowerCase();
-    }
-    // if player cancels or enters no input, ask player again
-    if (playerChoice === null) {
-        game();
-    }else if (playerChoice.length === 0 ) {
-        game()
-    }// start round if player made choice 
+// game starts when loading site
+game();
+function game() {
+    // ask player to make a choice through text input
+        playerChoice = prompt("Choose your move! Rock, Paper or Scissors?", "");
+    
+    // if player cancels, ask again, else adjust text cases from player input for the next check 
+    if (playerChoice === null) { game(); } 
     else { 
-     playerChoice = gameChoices.findIndex(choice => (playerChoice));
-     console.log(playerChoice);
+        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
     }
     
+    // check if player made choice out of the options, then start round
+    if (gameMoveOptions.findIndex(move => move.choice === playerChoice)!== -1){ 
+        playerChoice = gameMoveOptions.findIndex(move => move.choice === playerChoice);
+        playerChoice = gameMoveOptions[playerChoice];
+        playRound(playerChoice, getComputerChoice());
+    } // ask again if player input was incorret  
+    else { game();}
 }
 
+function playRound (playerChoice, computerChoice) {
+    // conditions for player winning a round
+    let roundWinCombo = `${playerChoice.value}-${computerChoice.value}`;
+    let playerWinCombo = ['0-2', '1-0', '2-1'];
+
+    // tie, when both chose the same move
+    if (playerChoice.value === computerChoice.value) {
+        alert(`Tie! You both chose ${playerChoice.choice}`);
+        game();
+    } // check if player met conditions for winning a round 
+    else if (playerWinCombo.includes(roundWinCombo)) {
+        alert(`You win! ${playerChoice.choice} beats ${computerChoice.choice}`)
+    } // if conditions not met, player loses the round
+    else {
+        alert(`You lose! ${computerChoice.choice} beats ${playerChoice.choice}`)}
+}
+
+// computer selecting random move
+function getComputerChoice() {
+    let computerChoiceResult = gameMoveOptions[Math.floor(Math.random() * gameMoveOptions.length)];   
+    return computerChoiceResult;
+}
