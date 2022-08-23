63f430231cd4fd21beefcbe2efe067a453f991f6
LavituScuperino <samet71simsek@gmail.com>
2022-08-23 21:48:28 +0200

    + added score count & display score + added round count, max 5 rounds & display current round

--- rock-paper-scissor-1.0.0.game.js
+++ rock-paper-scissor-1.0.0.game.js
@@ -4,51 +4,59 @@ let computerScore = 0;
 // rock paper scissors move options
 const gameMoveOptions = [{choice: 'Rock', value: 0}, {choice: 'Paper', value: 1}, {choice: 'Scissors', value: 2}];
 
-// game starts when loading site
-game();
-function game() {
-    // ask player to make a choice through text input
-        playerChoice = prompt("Choose your move! Rock, Paper or Scissors?", "");
-    
-    // if player cancels, ask again, else adjust text cases from player input for the next check 
-    if (playerChoice === null) { game(); } 
-    else { 
-        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-    }
-    
-    // check if player made choice out of the options, then start round
-    if (gameMoveOptions.findIndex(move => move.choice === playerChoice)!== -1){ 
-        playerChoice = gameMoveOptions.findIndex(move => move.choice === playerChoice);
-        playerChoice = gameMoveOptions[playerChoice];
-        playRound(playerChoice, getComputerChoice());
-    } // ask again if player input was incorret  
-    else { game();}
-}
-
-function playRound (playerChoice, computerChoice) {
-    // conditions for player winning a round
-    let roundWinCombo = `${playerChoice.value}-${computerChoice.value}`;
-    let playerWinCombo = ['0-2', '1-0', '2-1'];
-
-    // tie, when both chose the same move
-    if (playerChoice.value === computerChoice.value) {
-        alert(`Tie! You both chose ${playerChoice.choice}`);
-        game();
-    } // check if player met conditions for winning a round & if true, check if score met condition to win the game
-    else if (playerWinCombo.includes(roundWinCombo)) {
-        alert(`You win! ${playerChoice.choice} beats ${computerChoice.choice}`);
-        playerScore++;
-        checkScore
-    } // if conditions not met, player loses the round & if true, check if score met condition to win the game
-    else {
-        alert(`You lose! ${computerChoice.choice} beats ${playerChoice.choice}`);
-        computerScore++;
-    }
-
-}
-
-// computer selecting random move
+// computer selecting random move whenever function is called
 function getComputerChoice() {
     let computerChoiceResult = gameMoveOptions[Math.floor(Math.random() * gameMoveOptions.length)];   
     return computerChoiceResult;
+}
+
+//start game
+runRPSGame();
+function runRPSGame() {
+    // play 5 rounds
+    for (let r = 1;r < 6; r++) {
+        // start round
+        game();
+        function game() {
+            // ask player to make a choice through text input
+                playerChoice = prompt("Choose your move! Rock, Paper or Scissors?", "");
+            
+            // if player cancels, ask again, else adjust text cases from player input for the next check 
+            if (playerChoice === null) {game();} 
+            else { playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();}
+            
+            // check if player made choice out of the options,if yes continue the round
+            if (gameMoveOptions.findIndex(move => move.choice === playerChoice)!== -1){ 
+                playerChoice = gameMoveOptions.findIndex(move => move.choice === playerChoice);
+                playerChoice = gameMoveOptions[playerChoice];
+                playRound(playerChoice, getComputerChoice());
+            } // ask again if player input was incorret  
+            else { game();}
+        }
+
+        function playRound (playerChoice, computerChoice) {
+            // conditions for player winning a round
+            let roundWinCombo = `${playerChoice.value}-${computerChoice.value}`;
+            let playerWinCombo = ['0-2', '1-0', '2-1'];
+            // current round text & if last round text
+            let currentRound = `Round ${r}`;
+            
+            if (r === 5) {
+                currentRound = `Last Round ${r}` 
+            }
+            
+            // tie, when both chose the same move
+            if (playerChoice.value === computerChoice.value) {
+                alert(`Tie! You both chose ${playerChoice.choice}, ${currentRound}:  Player ${playerScore} - ${computerScore} Computer`);
+            } // check if player met conditions for winning a round & if true, give 1 point to player
+            else if (playerWinCombo.includes(roundWinCombo)) {
+                playerScore++;
+                alert(`You win! ${playerChoice.choice} beats ${computerChoice.choice}, ${currentRound}:  Player ${playerScore} - ${computerScore} Computer`);
+            } // if conditions not met, player loses the round & give 1 point to computer
+            else {
+                computerScore++;
+                alert(`You lose! ${computerChoice.choice} beats ${playerChoice.choice}, ${currentRound}:  Player ${playerScore} - ${computerScore} Computer`);
+            }
+        }
+    }
 }
